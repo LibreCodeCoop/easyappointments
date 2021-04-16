fff468d530db81464dd17c17d8e66686e6229f0b
Vitor Mattos <vitor@php.rio>
2021-04-16 17:14:41 -0300

    merge

--- assets/js/backend_settings_system.js
+++ assets/js/backend_settings_system.js
@@ -107,6 +107,11 @@
             value: $('#display-any-provider').prop('checked') ? '1' : '0'
         });
 
+        settings.push({
+            name: 'login_on_footer',
+            value: $('#login-on-footer').prop('checked') ? '1' : '0'
+        });
+
         // Business Logic Tab
 
         settings.push({
